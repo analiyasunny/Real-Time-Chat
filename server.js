2ef2d4fbda330fbfc766f2a37134cb8869842182
analiyasunny <analiyasunny06092004@gmail.com>
2025-04-02 23:42:58 -0400

    Bind to 0.0.0.0 for Render deployment

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+http.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
